84e8eb3ecd030b46ab2fab497da578d43d86f2cd
Sander Pool <poolsander97@gmail.com>
2019-12-11 15:16:45 +0100

    adjusted headers

--- routes/devyRouter.js
+++ routes/devyRouter.js
@@ -89,7 +89,7 @@ function routes(Devy){
         .options((req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
             res.sendStatus(200);
         });   
     return devyRouter;
