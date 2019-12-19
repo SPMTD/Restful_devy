276e1b09b8dfa99063f1a1581e84645d6cf2499c
Sander Pool <poolsander97@gmail.com>
2019-12-19 14:07:33 +0100

    added allow-header parameters

--- controllers/devysController.js
+++ controllers/devysController.js
@@ -80,15 +80,15 @@ function devysController(Devy) {
 
 
     function options(req, res, err) {
-        if (!res.header('Access-Control-Allow-Headers', 'Content-Type, Accept , Content-Type, Application/json, Content-Type, Application/x-www-form-urlencoded')) {
+        if (!res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Content-Type, Application/json, Content-Type, Application/x-www-form-urlencoded')) {
             res.sendStatus(416);
         } else {
             res.header('Allow-Methods', 'GET, POST, OPTIONS');
             res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-            res.header('Allow-Header', 'Content-Type, Accept')
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Content-Type, Application/json, Content-Type, Application/x-www-form-urlencoded');
+            res.header('Allow-Header', 'Content-Type, Accept, Content-Type, Application/json, Content-Type, Application/x-www-form-urlencoded')
             res.header('Access-Control-Allow-Content-Type', 'application/json,  x-www-form-urlencoded');
             res.header('Allow-Content-Type', 'application/json, x-www-form-urlencoded');
             res.header('Access-Control-Allow-Accept', 'application/json,  x-www-form-urlencoded');
